187ca963900e3bc08c31be8c148ffd260da3eab2
mbssilva <mbernardi.s@outlook.com>
2020-04-01 12:29:52 -0300

    Frontend da aplicação iniciado. Estrutura básica do projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Este será o front end da minha aplicação</h2>
+    </>
+  );
+}
+
+export default App;
