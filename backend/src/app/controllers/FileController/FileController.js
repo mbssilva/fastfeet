fd6dd83e5e2175b4cbf70ad8103b3f0945eb78a5
mbssilva <mbernardi.s@outlook.com>
2020-04-08 22:07:56 -0300

    Corrige uma importação que não deveria ocorrer e estava quebrando a aplicação

--- backend/src/app/controllers/FileController/FileController.js
+++ backend/src/app/controllers/FileController/FileController.js
@@ -1,15 +1,10 @@
 import * as Yup from 'yup';
 
 import storeFunction from './functions/storeFunction';
-import deleteFunction from './functions/deleteFunction';
 
 class DelivererController {
   async store(req, res) {
     return storeFunction(req, res);
-  }
-
-  async delete(req, res) {
-    return res.json({ok: true});
   }
 }
 
