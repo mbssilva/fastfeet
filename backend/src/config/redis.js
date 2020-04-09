108900e2e87f65fac4ea14c98ee84899ffe68835
mbssilva <mbernardi.s@outlook.com>
2020-04-08 22:04:24 -0300

    Adiciona ao arquivo de configuração do redis o Host e a Port

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,4 +1,6 @@
 export default {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  // host: process.env.REDIS_HOST,
+  // port: process.env.REDIS_PORT,
+  host: '127.0.0.1',
+  port: '6379',
 }
