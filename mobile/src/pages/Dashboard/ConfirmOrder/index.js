d206b4a5ff1181bccc625b2a9d104de1309a87d8
mbssilva <mbernardi.s@outlook.com>
2020-04-17 13:58:53 -0300

    Página de confirmação de entrega com integração da câmera estilizada. Câmera tirando fotos e forncencendo o objeto data

--- mobile/src/pages/Dashboard/ConfirmOrder/index.js
+++ mobile/src/pages/Dashboard/ConfirmOrder/index.js
@@ -1,13 +1,123 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import propTypes from 'prop-types';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { RNCamera } from 'react-native-camera';
 
 import Background from '../../../components/layouts/Signed';
 
-import { Container } from './styles';
+import { CameraContainer, SendButton } from './styles';
+
+function PendingView() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'lightgreen',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Waiting</Text>
+    </View>
+  );
+}
+
+function Trigger({ takePicture, camera }) {
+  return (
+    <View
+      style={{
+        flex: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          flex: 0,
+          backgroundColor: '#fff',
+          borderRadius: 5,
+          padding: 15,
+          paddingHorizontal: 20,
+          alignSelf: 'center',
+          margin: 20,
+        }}
+        onPress={() => takePicture(camera)}
+      >
+        <Text
+          style={{
+            fontSize: 14,
+            color: '#f4f',
+            fontWeight: 'bold',
+          }}
+        >
+          TIRAR FOTO
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function ConfirmOrder() {
-  return <Background />;
+  async function takePicture(camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    console.tron.warn(data.uri);
+  }
+
+  return (
+    <Background>
+      <CameraContainer>
+        <RNCamera
+          type={RNCamera.Constants.Type.back}
+          autoFocus
+          flashMode="off"
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}
+        >
+          {({ camera, status, recordAudioPermissionStatus }) =>
+            status !== 'READY' ? (
+              <PendingView />
+            ) : (
+              <Trigger
+                takePicture={() => {
+                  takePicture(camera);
+                }}
+                camera={camera}
+              />
+            )
+          }
+        </RNCamera>
+
+        <SendButton onPress={() => {}}>
+          <Text
+            style={{
+              fontSize: 14,
+              color: '#fff',
+              fontWeight: 'bold',
+            }}
+          >
+            ENVIAR FOTO
+          </Text>
+        </SendButton>
+      </CameraContainer>
+    </Background>
+  );
 }
 
 ConfirmOrder.navigationOptions = ({ navigation }) => {
--- mobile/src/pages/Dashboard/ConfirmOrder/styles.js
+++ mobile/src/pages/Dashboard/ConfirmOrder/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const CameraContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  padding: 60px 20px 0;
+`;
+
+export const SendButton = styled.TouchableOpacity`
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  align-self: stretch;
+  margin: 20px 0;
+  padding: 15px 0;
+  border-radius: 5px;
+`;
